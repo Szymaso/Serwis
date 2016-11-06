20d1f38793f4c24d121ee7943997ab1412b6ef68
Szymon Darmofał <Szymon Darmofa>
2016-11-06 10:25:35 +0100

    Plik readme

---
+++ Serwis/Home.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Serwis
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listUsersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.homePanel = new System.Windows.Forms.Panel();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(67, 4);
+            // 
+            // menu
+            // 
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.userToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(1196, 28);
+            this.menu.TabIndex = 3;
+            this.menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zamknijProgramToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "Plik";
+            // 
+            // zamknijProgramToolStripMenuItem
+            // 
+            this.zamknijProgramToolStripMenuItem.Name = "zamknijProgramToolStripMenuItem";
+            this.zamknijProgramToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.zamknijProgramToolStripMenuItem.Text = "Zamknij program";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewToolStripMenuItem,
+            this.listUsersToolStripMenuItem});
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.userToolStripMenuItem.Text = "Użytkownik";
+            // 
+            // addNewToolStripMenuItem
+            // 
+            this.addNewToolStripMenuItem.Name = "addNewToolStripMenuItem";
+            this.addNewToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.addNewToolStripMenuItem.Text = "Nowy";
+            this.addNewToolStripMenuItem.Visible = false;
+            // 
+            // listUsersToolStripMenuItem
+            // 
+            this.listUsersToolStripMenuItem.Name = "listUsersToolStripMenuItem";
+            this.listUsersToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.listUsersToolStripMenuItem.Text = "Lista ";
+            this.listUsersToolStripMenuItem.Visible = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // homePanel
+            // 
+            this.homePanel.BackColor = System.Drawing.SystemColors.Control;
+            this.homePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.homePanel.Location = new System.Drawing.Point(0, 0);
+            this.homePanel.Name = "homePanel";
+            this.homePanel.Size = new System.Drawing.Size(1196, 447);
+            this.homePanel.TabIndex = 4;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 447);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.homePanel);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijProgramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listUsersToolStripMenuItem;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Panel homePanel;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Aplikacja wspierająca funkcjonowanie serwisu sprzętu elektronicznego
+
+## Podstawowe funkcjonalności:
+1. Konta użytkowników.
+2. Dodawanie nowego sprzętu na stan serwisowy.
+3. Drukowanie potwierdzenia przyjęcia sprzętu dla klienta.
+4. Karty sprzętu (właściciel, nazwa, typ, numer seryjny, producent).
+5. Karty naprawy (ściśle powiązana z kartą sprzętu zawierająca również opis uszkodzenia, diagnoza, co ostatecznie zostało wymienione, czas gwarancji, data przyjęcia urządzenia do serwisu, data odbioru urządzenia z serwisu)
+6. Dane o klientach umożliwiające wyliczanie rabatu.
+7. Lista urządzeń na stanie serwisowym wraz z filtrowaniem (do naprawy, naprawione, wystawiona diagnoza, wyszukaj po numerze zlecenia, wyszukaj po danych klienta, wyszukiwanie po danych urządzenia).
+8. Wysyłka maili o zmianie statusów (przyjęcie do serwisu, wystawiona diagnoza, gotowy do odbioru, odebrany z serwisu).
